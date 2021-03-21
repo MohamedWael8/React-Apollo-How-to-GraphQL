90969b9f657757d486e20d95e756f01162f4e08c
Mohamed Wa'el <mwael8@outlook.com>
2021-03-21 12:54:35 +0200

    Links Login & Authentication

--- apollo-react/src/index.js
+++ apollo-react/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
+import { AUTH_TOKEN } from './constants';
 
 import {
   ApolloProvider,
@@ -13,12 +16,27 @@ import {
 
 const httpLink = createHttpLink({ uri: "http://localhost:4000" });
 
-const client = new ApolloClient({ link: httpLink, cache: new InMemoryCache() });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
